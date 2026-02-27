f88658c2a91bd3ebe3be34aa51b5ebe524c5fc50
gkiviv <georgkivivali7@gmail.com>
2026-02-27 19:16:46 +0200

    removed irrelevant stuff

--- rust/flake.nix
+++ rust/flake.nix
@@ -18,53 +18,8 @@
       in {
         devShells.default = with pkgs;
           mkShell {
-            buildInputs = [
-              openssl
-              pkg-config
-              eza
-              fd
-              rust-bin.beta.latest.default
-              rust-analyzer
-              clippy
-              libxkbcommon
-              lz4
-              wayland-protocols
-              wayland
+            buildInputs = [ rust-bin.beta.latest.default rust-analyzer clippy ];
 
-              python312Packages.fastapi
-              python312Packages.uvicorn
-              python312Packages.transformers
-              python312Packages.torch
-              python312Packages.accelerate
-              python312Packages.diffusers
-              python312Packages.pillow
-              python312Packages.pydantic
-              basedpyright
-              pkgs.python312Packages.black
-              pkgs.python312Packages.pyserial
-              pkgs.python312Packages.scipy
-              pkgs.python312Packages.pip
-              intel-graphics-compiler
-              mkl
-            ];
-
-            shellHook = ''
-              alias ls=eza
-              alias find=fd
-            '';
           };
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-          pkgs.mkl
-          pkgs.intel-ocl
-          pkgs.intel-media-driver
-          pkgs.intel-compute-runtime
-          pkgs.level-zero
-          pkgs.vpl-gpu-rt
-          pkgs.openssl
-          pkgs.zlib
-          pkgs.stdenv.cc.cc.lib
-        ];
-        PKG_CONFIG_PATH =
-          "${pkgs.wayland.dev}/lib/pkgconfig:${pkgs.wayland-protocols}/share/pkgconfig";
       });
 }
